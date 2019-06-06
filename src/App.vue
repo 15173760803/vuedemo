bfaf2a6718f9138d1155b11adc8cbd05b3543a91
15173760803 <2435118603@qq.com>
2019-06-07 04:08:46 +0800

    vue手练项目

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+      <Div></Div>
+    <router-view/>
+  </div>
+</template>
+<script>
+import Div from './components/div'
+export default {
+  name: 'app',
+  components:{
+    Div
+  }
+}
+</script>
+
+<style>
+*{
+  margin: 0px;
+  padding:0px;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+
+}
+</style>
+
+
---
+++ src/pages/xiaozu.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+      小组
+    </div>
+</template>
